6ec49039bbd4a0a5fe839b81b4565739223a1790
Alex <oleksii.pidhornyi@gmail.com>
2025-01-14 21:15:20 +0100

    Initial Commit

---
+++ WeatherSeasons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherSeasons
+//
+//  Created by Oleksii on 14.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherSeasons/WeatherSeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSeasonsApp.swift
+//  WeatherSeasons
+//
+//  Created by Oleksii on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
